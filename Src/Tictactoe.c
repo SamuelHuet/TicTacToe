dd1c21886689b72bd9d254c9efdac09dbd679f24
Huet Samuel <contact@huetsamuel.fr>
2020-04-13 22:28:54 +0200

    Add printing grid function
    Signed-off-by: Huet Samuel <contact@huetsamuel.fr>

--- Src/Tictactoe.c
+++ Src/Tictactoe.c
@@ -1,8 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "Tictactoe.h"
+
+void print_grid(unsigned char line1[], unsigned char line2[], unsigned char line3[]){
+  printf("-------------------\r\n");
+  printf("|  %c  |  %c  |  %c  |\r\n", line1[0], line1[1], line1[2]);
+  printf("-------------------\r\n");
+  printf("|  %c  |  %c  |  %c  |\r\n", line2[0], line2[1], line2[2]);
+  printf("-------------------\r\n");
+  printf("|  %c  |  %c  |  %c  |\r\n", line3[0], line3[1], line3[2]);
+  printf("-------------------\r\n");
+}
+
+unsigned char line1 [3] = {' ', ' ', ' '};
+unsigned char line2 [3] = {' ', ' ', ' '};
+unsigned char line3 [3] = {' ', ' ', ' '};
+
+
 
 int main(int argc, char *argv[])
 {
-    printf("Hello world!\n");
+    print_grid(&line1, &line2, &line3);
     return 0;
 }
---
+++ Src/Tictactoe.h
@@ -0,0 +1,6 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+void print_grid(unsigned char *line1, unsigned char *line2, unsigned char *line3);
